ce78f2a68ed523e10789cf25fc76b36f65e25d5d
syed dilawar <wasimdilawer@gmail.com>
2020-11-07 20:04:28 -0800

    Initial commit

---
+++ src/components/PDF.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
+const PDF = (props) => {
+  return (
+    <>
+      <div className="Post" ref={ref}>
+        <h1>{props.title}</h1>
+        <img src={props.image} alt={props.title}></img>
+        <p>{props.content}</p>
+      </div>
+      <Pdf targetRef={ref} filename="Post.pdf">
+        {({ toPdf }) => <button onClick={toPdf}>Save your Post</button>}
+      </Pdf>
+    </>
+  );
+};
+export default PDF;
